8507ee3759d824d22268dc27d7d0d5157668c474
Harsh Ranjan <78690617+harshranjan63@users.noreply.github.com>
2021-05-15 00:44:43 +0530

    #Task 2 done

---
+++ Web Development/Task-2/Task submission/Harsh Ranjan/javascript.js
@@ -0,0 +1,12 @@
+function clickme()
+{
+    console.log('I Ws clices');
+    document.getElementsByTagName("body")[0].style.background = "yellow";
+    document.getElementsByTagName("header")[0].style.color="blue";
+    document.getElementsByClassName("container2")[0].style.color="rgb(81, 163, 122)";
+    document.getElementsByClassName("container2")[1].style.color="rgb(81, 163, 122)";
+    document.getElementsByClassName("container2")[2].style.color="rgb(81, 163, 122)";
+    document.getElementsByClassName("container2")[3].style.color="rgb(81, 163, 122)";    
+    document.getElementsByClassName("container4")[0].style.color="rgb(163,81,81)";
+    document.getElementsByClassName("container4")[1].style.color="rgb(163,81,81)";
+}
