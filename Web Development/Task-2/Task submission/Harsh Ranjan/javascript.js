68d566a2bfbca8e72e3be62c6afd0d5f61e803eb
Harsh Ranjan <78690617+harshranjan63@users.noreply.github.com>
2021-05-15 12:14:25 +0530

    update 3

--- Web Development/Task-2/Task submission/Harsh Ranjan/javascript.js
+++ Web Development/Task-2/Task submission/Harsh Ranjan/javascript.js
@@ -1,4 +1,21 @@
+var a;
 function clickme()
+{
+    if(a==1)
+    {
+        console.log('I Ws clices2');
+    document.getElementsByTagName("body")[0].style.background = "aqua";
+    document.getElementsByTagName("header")[0].style.color="rgb(238, 42, 42)";
+    document.getElementsByClassName("container2")[0].style.color="rgb(163,81,81)";
+    document.getElementsByClassName("container2")[1].style.color="rgb(163,81,81)";
+    document.getElementsByClassName("container2")[2].style.color="rgb(163,81,81)";
+    document.getElementsByClassName("container2")[3].style.color="rgb(163,81,81)";    
+    document.getElementsByClassName("container4")[0].style.color="rgb(81, 163, 122)";
+    document.getElementsByClassName("container4")[1].style.color="rgb(81, 163, 122)";
+    return a=0;
+    
+    }
+    else 
 {
     console.log('I Ws clices');
     document.getElementsByTagName("body")[0].style.background = "yellow";
@@ -9,4 +26,6 @@ function clickme()
     document.getElementsByClassName("container2")[3].style.color="rgb(81, 163, 122)";    
     document.getElementsByClassName("container4")[0].style.color="rgb(163,81,81)";
     document.getElementsByClassName("container4")[1].style.color="rgb(163,81,81)";
+    return a=1;
+}
 }
