ea7bddb0443d5c8f8607591f60bec8ef3a186506
it5rv <it5rv@users.noreply.github.com>
2021-05-22 12:57:11 +0530

    removed extra files

--- Python and Ml/Task-1/Task Submission/Ravindra-Nag/randomSum.py
+++
@@ -1,7 +0,0 @@
-import random
-sum=0
-for i in range(10):
-    n = random.randint(0,100)
-    sum+=n
-
-print('Sum of 10 random numbers: ',sum)
--- Python and Ml/Task-1/Task Submission/Ravindra-Nag/sortWords.py
+++
@@ -1,5 +0,0 @@
-sen = input('Enter a sentence: ')
-list=sen.split()
-list.sort()
-for i in range(len(list)):
-    print(list[i])
