1e7c27257a57dd8a0323ebf19f199e096de0adf6
Prasiddhi-IT <78656160+Prasiddhi-IT@users.noreply.github.com>
2021-05-23 14:05:26 +0530

    Delete Python and Ml/Task-1/Task Submission/prasiddhi-sahu directory

--- Python and Ml/Task-1/Task Submission/prasiddhi-sahu/code4.py
+++
@@ -1,8 +0,0 @@
-# input
-my_str = input("Enter a string: ")
- 
-words = my_str.split()
-words.sort()
- 
-for word in words:
-   print(word)
--- Python and Ml/Task-1/Task Submission/prasiddhi-sahu/code5.py
+++
@@ -1,8 +0,0 @@
-import random
-
-randomlist=random.sample(range(1, 100), 10)
-print(randomlist)
-    
-sum = sum(randomlist)
-  
-print ("Random number summation list is : " + str(sum))
