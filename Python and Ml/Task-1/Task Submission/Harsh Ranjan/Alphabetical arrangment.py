3a0857c8c7c36309f13235eeec3a1938612c1a1a
Harsh Ranjan <78690617+harshranjan63@users.noreply.github.com>
2021-05-23 15:35:56 +0530

    #task 1 update

--- Python and Ml/Task-1/Task Submission/Harsh Ranjan/Alphabetical arrangment.py
+++
@@ -1,6 +0,0 @@
-my_str = "Hi everyone, I am Harsh Ranjan"
-words = [word.lower() for word in my_str.split()]
-words.sort()
-print("The words shorted in alphabetical order are:")
-for word in words:
-   print(word)
--- Python and Ml/Task-1/Task Submission/Harsh Ranjan/random number.py
+++
@@ -1,7 +0,0 @@
-import random
-sum=0
-for i in range (0,10):
-    print(sum)
-    sum=sum+random.random()
-
-print("The sum of 10 random numbers generated is: ",sum)
